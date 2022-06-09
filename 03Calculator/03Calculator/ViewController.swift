a92bc51bacdae88e7cbc6f3c9449a6717ff9f263
goodgeon <dlrnerjs@gmail.com>
2022-06-09 18:41:38 +0900

    [03Calculator] calculating

--- 03Calculator/03Calculator/ViewController.swift
+++ 03Calculator/03Calculator/ViewController.swift
@@ -6,26 +6,115 @@
 //
 
 import UIKit
+enum Operation {
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case unknown
+}
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var button7: RoundButton!
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var result = ""
+    var currentOperation: Operation = .unknown
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("viewDidLoad")
+        
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        print("viewDidAppear")
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        guard let numberValue = sender.title(for: .normal) else { return }
+                
+        if displayNumber.count < 9 {
+            displayNumber += numberValue
+            numberOutputLabel.text = displayNumber
+        }
     }
     
-    override func viewDidLayoutSubviews() {
-        print("viewDidLayoutSubviews")
-//        button7.layer.cornerRadius = button7.frame.height / 2
-        button7.isRound = true
+    @IBAction func tapClearButton(_ sender: UIButton) {
+        displayNumber = ""
+        firstOperand = ""
+        secondOperand = ""
+        result = ""
+        numberOutputLabel.text = "0"
+        currentOperation = .unknown
     }
+    
+    @IBAction func tapDotButton(_ sender: UIButton) {
+        if displayNumber.count < 8, !displayNumber.contains(".") {
+            displayNumber += self.displayNumber.isEmpty ? "0." : "."
+            numberOutputLabel.text = displayNumber
+        }
+    }
+    
+    @IBAction func tapDivideButton(_ sender: UIButton) {
+        operation(.Divide)
+    }
+    
+    @IBAction func tapMultiplyButton(_ sender: UIButton) {
+        operation(.Multiply)
+    }
+    
+    @IBAction func tapSubtractButton(_ sender: UIButton) {
+        operation(.Subtract)
+    }
+    
+    @IBAction func tapPlusButton(_ sender: UIButton) {
+        operation(.Add)
+    }
+    
+    @IBAction func tapEqualButton(_ sender: UIButton) {
+        operation(currentOperation)
+    }
+    
+    func operation(_ operation: Operation) {
+        if currentOperation != .unknown {
+            if !displayNumber.isEmpty {
+                secondOperand = displayNumber
+                displayNumber = ""
 
+                guard let firstOperand = Double(firstOperand) else { return }
+                guard let secondOperand = Double(secondOperand) else { return }
+                
+                switch(currentOperation) {
+                case .Add:
+                    result = "\(firstOperand + secondOperand)"
+                case .Subtract:
+                    result = "\(firstOperand - secondOperand)"
+                case .Divide:
+                    result = "\(firstOperand / secondOperand)"
+                case .Multiply:
+                    result = "\(firstOperand * secondOperand)"
+                case .unknown:
+                    break
+                }
 
+                
+                if let result = Double(result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+                
+                self.firstOperand = result
+                currentOperation = .unknown
+                numberOutputLabel.text = result
+                return
+            }
+            
+            currentOperation = operation
+        } else {
+            if !displayNumber.isEmpty {
+                firstOperand = displayNumber
+                currentOperation = operation
+                displayNumber = ""
+            }
+            
+        }
+    }
 }
 
