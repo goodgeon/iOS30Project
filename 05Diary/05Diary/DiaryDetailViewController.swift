6feac844c25a51301ec53115f93c7669c4c03781
goodgeon <dlrnerjs@gmail.com>
2022-06-15 18:22:51 +0900

    .

--- 05Diary/05Diary/DiaryDetailViewController.swift
+++ 05Diary/05Diary/DiaryDetailViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 protocol DiaryDetailViewDelegate: AnyObject {
     func didSelectDelete(indexPath: IndexPath)
+    func didSelectStar(indexPath: IndexPath, isStar: Bool)
 }
 
 class DiaryDetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var dateLabel: UILabel!
+    var starButton: UIBarButtonItem?
+    
     weak var delegate: DiaryDetailViewDelegate?
     
     var diary: Diary?
@@ -30,6 +33,10 @@ class DiaryDetailViewController: UIViewController {
         titleLabel.text = diary.title
         contentsTextView.text = diary.contents
         dateLabel.text = dateToString(date: diary.date)
+        starButton = UIBarButtonItem(image: nil, style: .plain, target: self, action: #selector(tapStarButton))
+        starButton?.image = diary.isStar ? UIImage(systemName: "star.fill") : UIImage(systemName: "star")
+        starButton?.tintColor = UIColor.orange
+        self.navigationItem.rightBarButtonItem = starButton
     }
     
     private func dateToString(date: Date) -> String {
@@ -64,6 +71,19 @@ class DiaryDetailViewController: UIViewController {
         guard let indexPath = self.indexPath else { return }
         self.delegate?.didSelectDelete(indexPath: indexPath)
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc private func tapStarButton() {
+        guard let isStar = self.diary?.isStar else { return }
+        guard let indexPath = self.indexPath else { return }
+        if isStar {
+            self.starButton?.image = UIImage(systemName: "star")
+        } else {
+            self.starButton?.image = UIImage(systemName: "star.fill")
+        }
+        
+        self.diary?.isStar = !isStar
+        self.delegate?.didSelectStar(indexPath: indexPath, isStar: self.diary?.isStar ?? false)
     }
     
     deinit {
--- 05Diary/05Diary/StarViewController.swift
+++ 05Diary/05Diary/StarViewController.swift
@@ -8,22 +8,75 @@
 import UIKit
 
 class StarViewController: UIViewController {
-
+    var diaryList = [Diary]()
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureCollectionView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadStarDiaryList()
     }
-    */
+    
+    private func configureCollectionView() {
+        self.collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        self.collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+    }
+    
+    private func loadStarDiaryList() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "diaryList") as? [[String: Any]] else { return }
+        
+        self.diaryList = data.compactMap {
+            guard let title = $0["title"] as? String else { return nil }
+            guard let contents = $0["contents"] as? String else { return nil }
+            guard let date = $0["date"] as? Date else { return nil }
+            guard let isStar = $0["isStar"] as? Bool else { return nil }
+            
+            return Diary(title: title, contents: contents, date: date, isStar: isStar)
+        }.filter {
+            $0.isStar
+        }.sorted {
+            $0.date.compare($1.date) == .orderedDescending
+        }
+        self.collectionView.reloadData()
+    }
+    
+    private func dateToString(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        
+        return formatter.string(from: date)
+    }
+}
 
+extension StarViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width - 20, height: 80)
+    }
+}
+
+extension StarViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        diaryList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "StarCell", for: indexPath) as? StarCell else { return UICollectionViewCell() }
+        
+        let diary = self.diaryList[indexPath.row]
+        
+        cell.titleLabel.text = diary.title
+        cell.dateLabel.text = self.dateToString(date: diary.date)
+        
+        return cell
+    }
+    
+    
 }
