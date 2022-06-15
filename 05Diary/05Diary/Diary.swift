5aaf78e1bb1ef073c0e9c741b47f77a5b699e67d
goodgeon <dlrnerjs@gmail.com>
2022-06-15 14:08:39 +0900

    [05Diary] Adding Diary

---
+++ 05Diary/05Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  05Diary
+//
+//  Created by 李 グッゴン on 2022/06/15.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
--- 05Diary/05Diary/WriteDiaryViewController.swift
+++ 05Diary/05Diary/WriteDiaryViewController.swift
@@ -7,23 +7,94 @@
 
 import UIKit
 
+protocol WriteDiaryViewDelegate: AnyObject {
+    func didSelectRegister(diary: Diary)
+}
+
 class WriteDiaryViewController: UIViewController {
 
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var contentsTextView: UITextView!
+    @IBOutlet var dateTextField: UITextField!
+    
+    private let datePicker = UIDatePicker()
+    private var diaryDate: Date?
+    weak var delegate: WriteDiaryViewDelegate?
+    
+    @IBOutlet var confirmButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.confirmButton.isEnabled = false
+        self.configureContentsTextView()
+        self.configureDateTextField()
+        self.configureInputField()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureContentsTextView() {
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1)
+        contentsTextView.layer.borderColor = borderColor.cgColor
+        contentsTextView.layer.borderWidth = 0.5
+        contentsTextView.layer.cornerRadius = 5.0
     }
-    */
+    
+    private func configureDateTextField() {
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .wheels
+        self.datePicker.addTarget(self, action: #selector(datePickerValueDidChange), for: .valueChanged)
+        self.datePicker.locale = Locale(identifier: "ko_KR")
+        self.dateTextField.inputView = self.datePicker
+    }
+    
+    private func configureInputField() {
+        contentsTextView.delegate = self
+        titleTextField.addTarget(self, action: #selector(titleTextFieldDidChange(_:)), for: .editingChanged)
+        dateTextField.addTarget(self, action: #selector(dateTextFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    private func validateInputField() {
+        confirmButton.isEnabled = !(titleTextField.text?.isEmpty ?? true) && !contentsTextView.text.isEmpty && !(dateTextField.text?.isEmpty ?? true)
+    }
+    
+    @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
+        guard let title = self.titleTextField.text else { return }
+        guard let contents = self.contentsTextView.text else { return }
+        guard let date = self.diaryDate else { return }
+        
+        let diary = Diary(title: title, contents: contents, date: date, isStar: false)
+        
+        self.delegate?.didSelectRegister(diary: diary)
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc
+    private func datePickerValueDidChange(_ datePicker: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        self.diaryDate = datePicker.date
+        dateTextField.text = formatter.string(from: datePicker.date)
+        self.dateTextField.sendActions(for: .editingChanged)
+    }
+    
+    @objc
+    private func titleTextFieldDidChange(_ textField: UITextField) {
+        self.validateInputField()
+    }
+    
+    @objc
+    private func dateTextFieldDidChange(_ textField: UITextField) {
+        self.validateInputField()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
 
+extension WriteDiaryViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        self.validateInputField()
+    }
 }
