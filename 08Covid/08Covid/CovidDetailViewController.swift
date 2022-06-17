540272c21f22d42ba4ed2f3dbe8c7834f4d06925
goodgeon <dlrnerjs@gmail.com>
2022-06-17 11:22:37 +0900

    [08Covid] UI

---
+++ 08Covid/08Covid/CovidDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CovidDetailViewController.swift
+//  08Covid
+//
+//  Created by 李 グッゴン on 2022/06/17.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ 08Covid/08Covid/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  08Covid
+//
+//  Created by 李 グッゴン on 2022/06/17.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController {
+    @IBOutlet weak var totalCaseLabel: UILabel!
+    @IBOutlet weak var newCaseLabel: UILabel!
+    
+    @IBOutlet weak var pieChartView: PieChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
