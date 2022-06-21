4786898d73b4afdfb44cf6ad907b4dfaa7ac7e9d
goodgeon <dlrnerjs@gmail.com>
2022-06-22 08:08:21 +0900

    .

--- 10CreditCardList/10CreditCardList/CardListViewController.swift
+++ 10CreditCardList/10CreditCardList/CardListViewController.swift
@@ -8,8 +8,11 @@
 import Foundation
 import UIKit
 import Kingfisher
+import FirebaseDatabase
 
 class CardListViewController: UITableViewController {
+    var ref: DatabaseReference!
+    
     var creditCardList: [CreditCard] = []
     
     override func viewDidLoad() {
@@ -17,9 +20,28 @@ class CardListViewController: UITableViewController {
         
         let nibName = UINib(nibName: "CardListCell", bundle: nil)
         tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+        
+        ref = Database.database().reference()
+        
+        ref.observe(.value, with: {[weak self] snapshot in
+            guard let value = snapshot.value as? [String: [String:Any]] else { return }
+            
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: value)
+                let cardData = try JSONDecoder().decode([String: CreditCard].self, from: jsonData)
+                let cardList = Array(cardData.values)
+                self?.creditCardList = cardList.sorted { $0.rank < $1.rank }
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            } catch let error {
+                print("ERROR JSON parsing \(error.localizedDescription)")
+            }
+        })
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return creditCardList.count
     }
     
@@ -27,7 +49,7 @@ class CardListViewController: UITableViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell", for: indexPath) as? CardListCell else { return UITableViewCell() }
         
         cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
-        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail)만원 증정"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
         cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
         
         let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
@@ -42,5 +64,9 @@ class CardListViewController: UITableViewController {
         
         detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail
         self.show(detailViewController, sender: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }
