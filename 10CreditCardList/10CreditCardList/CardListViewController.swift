d270769e53f3d6ae1a1b25f7e94222f2eae06c5a
goodgeon <dlrnerjs@gmail.com>
2022-06-22 17:18:16 +0900

    [10CreditCard] Firebase Firestore

--- 10CreditCardList/10CreditCardList/CardListViewController.swift
+++ 10CreditCardList/10CreditCardList/CardListViewController.swift
@@ -9,9 +9,11 @@ import Foundation
 import UIKit
 import Kingfisher
 import FirebaseDatabase
+import FirebaseFirestore
 
 class CardListViewController: UITableViewController {
-    var ref: DatabaseReference!
+//    var ref: DatabaseReference!
+    var db = Firestore.firestore()
     
     var creditCardList: [CreditCard] = []
     
@@ -21,24 +23,50 @@ class CardListViewController: UITableViewController {
         let nibName = UINib(nibName: "CardListCell", bundle: nil)
         tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
         
-        ref = Database.database().reference()
+//        ref = Database.database().reference()
+//
+//        ref.observe(.value, with: {[weak self] snapshot in
+//            guard let value = snapshot.value as? [String: [String:Any]] else { return }
+//
+//            do {
+//                let jsonData = try JSONSerialization.data(withJSONObject: value)
+//                let cardData = try JSONDecoder().decode([String: CreditCard].self, from: jsonData)
+//                let cardList = Array(cardData.values)
+//                self?.creditCardList = cardList.sorted { $0.rank < $1.rank }
+//
+//                DispatchQueue.main.async {
+//                    self?.tableView.reloadData()
+//                }
+//            } catch let error {
+//                print("ERROR JSON parsing \(error.localizedDescription)")
+//            }
+//        })
         
-        ref.observe(.value, with: {[weak self] snapshot in
-            guard let value = snapshot.value as? [String: [String:Any]] else { return }
-            
-            do {
-                let jsonData = try JSONSerialization.data(withJSONObject: value)
-                let cardData = try JSONDecoder().decode([String: CreditCard].self, from: jsonData)
-                let cardList = Array(cardData.values)
-                self?.creditCardList = cardList.sorted { $0.rank < $1.rank }
-                
-                DispatchQueue.main.async {
-                    self?.tableView.reloadData()
-                }
-            } catch let error {
-                print("ERROR JSON parsing \(error.localizedDescription)")
+        
+        //Firestore
+        db.collection("creditCardList").addSnapshotListener { snapshot, error in
+            guard let documents = snapshot?.documents else {
+                print("ERROR Firestore fetching document \(String(describing: error))")
+                return
             }
-        })
+            
+            self.creditCardList = documents.compactMap { doc -> CreditCard? in
+                do {
+                    let jsonData = try JSONSerialization.data(withJSONObject: doc.data(), options: [])
+                    let creditCard = try JSONDecoder().decode(CreditCard.self, from: jsonData)
+                    return creditCard
+                }catch let error {
+                    print("ERROR JSON Parsing \(error.localizedDescription)")
+                    return nil
+                }
+            }.sorted(by: {
+                $0.rank < $1.rank
+            })
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -65,13 +93,30 @@ class CardListViewController: UITableViewController {
         detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail
         self.show(detailViewController, sender: nil)
         
-        ref.queryOrdered(byChild: "id").queryEqual(toValue: creditCardList[indexPath.row].id).observe(.value, with: {[weak self] snapshot in
-            guard let self = self,
-                  let value = snapshot.value as? [String: [String: Any]],
-                  let key = value.keys.first else { return }
+        //realtime database
+//        ref.queryOrdered(byChild: "id").queryEqual(toValue: creditCardList[indexPath.row].id).observe(.value, with: {[weak self] snapshot in
+//            guard let self = self,
+//                  let value = snapshot.value as? [String: [String: Any]],
+//                  let key = value.keys.first else { return }
+//
+//            self.ref.child("\(key)").child("isSelected").setValue(true)
+//        })
+        
+        //Firestore
+        //option1
+        let cardID = creditCardList[indexPath.row].id
+//        db.collection("creditCardList").document("card\(cardID)").updateData(["isSelected": true])
+        
+        //option2
+        db.collection("creditCardList").whereField("id", isEqualTo: cardID).getDocuments { snapshot, _ in
+            guard let document = snapshot?.documents.first else {
+                print("ERROR Firestore fetching document")
+                return
+            }
             
-            self.ref.child("\(key)").child("isSelected").setValue(true)
-        })
+            document.reference.updateData(["isSelected":true])
+            
+        }
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -84,12 +129,28 @@ class CardListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            ref.queryOrdered(byChild: "id").queryEqual(toValue: creditCardList[indexPath.row].id).observe(.value, with: {[weak self] snapshot in
-                guard let self = self,
-                      let value = snapshot.value as? [String: [String: Any]],
-                      let key = value.keys.first else { return }
-                
-                self.ref.child(key).removeValue()
+            //realtime database
+//            ref.queryOrdered(byChild: "id").queryEqual(toValue: creditCardList[indexPath.row].id).observe(.value, with: {[weak self] snapshot in
+//                guard let self = self,
+//                      let value = snapshot.value as? [String: [String: Any]],
+//                      let key = value.keys.first else { return }
+//
+//                self.ref.child(key).removeValue()
+//            })
+            
+            //Firestore
+            //option1
+            let cardID = creditCardList[indexPath.row].id
+//            db.collection("creditCardList").document("card\(cardID)").delete()
+            
+            //option2
+            db.collection("creditCardList").whereField("id", isEqualTo: cardID).getDocuments(completion: { snapshot, _ in
+                guard let document = snapshot?.documents.first else {
+                    print("ERROR")
+                    return
+                }
+
+                document.reference.delete()
             })
         }
     }
