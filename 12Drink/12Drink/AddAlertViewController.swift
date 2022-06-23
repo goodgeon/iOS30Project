a5ceac42dacdb469d81509271f6e34efb200e79f
goodgeon <dlrnerjs@gmail.com>
2022-06-23 16:15:48 +0900

    [12Drink] UI

---
+++ 12Drink/12Drink/AddAlertViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddAlertViewController.swift
+//  12Drink
+//
+//  Created by 李 グッゴン on 2022/06/23.
+//
+
+import Foundation
+import UIKit
+
+class AddAlertViewController: UIViewController {
+    var pickedDate: ((_ date: Date) -> Void)?
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func dissmissButtonTapped(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        pickedDate?(datePicker.date)
+        self.dismiss(animated: true, completion: nil)
+    }
+}
