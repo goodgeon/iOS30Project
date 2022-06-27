b369f64a145b54538e9146c154af3754f9ff9a57
goodgeon <dlrnerjs@gmail.com>
2022-06-27 23:18:20 +0900

    [13PushNotification] Project setup & Firebase configure

---
+++ 13PushNotification/13PushNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  13PushNotification
+//
+//  Created by Good geon Lee on 2022/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
